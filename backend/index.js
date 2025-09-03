45f35b296eaeb2a0650db6523b70fea027ab5d6b
byam11123 <kaiwartyabyamkesh7@gmail.com>
2025-09-03 13:22:13 +0530

    routes done

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 const port = 5000;
@@ -7,6 +12,9 @@ app.get("/", (req, res) => {
   res.send("Hello, I am your Backend");
 });
 
+app.use("/api/v1/auth", authRoutes);
+
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is listening on http://localhost:5000`);
 });
